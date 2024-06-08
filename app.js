bf100c1d57b4939cee6268a8f4d9a8d3bee1234c
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-08 08:05:10 +0200

    delete userroute

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const bodyParser = require('body-parser');
 const { engine } = require('express-handlebars');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -52,7 +51,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
