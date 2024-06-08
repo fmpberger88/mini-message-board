bd17caf824555898319f09f97f8d8a4b930284ee
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-08 07:48:50 +0200

    add model message

---
+++ models/messages.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const { body, validationResult } = require('express-validator');
+
+const messageSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: [true, 'Message text is required'],
+        trim: true,
+        minlength: [1, 'Message text must be at least 1 character long']
+    },
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        trim: true,
+        minlength: [1, 'Username must be at least 1 character long']
+    },
+    added: {
+        type: Date,
+        default: Date.now,
+        required: [true, 'Date is required']
+    }
+});
+
+module.exports = mongoose.model('Message', messageSchema);
