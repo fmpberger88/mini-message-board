042e7d1b018f5285956e8f83f733863e76a45c8c
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-08 08:05:24 +0200

    adding post routes for creating messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,47 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const Message = require('../models/messages');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    const messages = Message.find({});
+    if (err) {
+        console.error(err);
+    } else {
+        res.render('index', { title: 'Message Board', messages: messages });
+    }
 });
+
+// New message
+router.post('/new', [
+    body('username')
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('Username is required')
+        .escape(),
+    body('text')
+        .trim()
+        .isLength({ min: 1 })
+        .withMessage('Text is required')
+        .escape()
+], async function(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const messages = await Message.find({});
+        return res.render('index', {
+            title: 'Mini Message Board',
+            messages: messages,
+            errors: errors.array()
+        });
+    }
+
+    const { username, text } = req.body;
+    const message = new Message({ username, text });
+    await message.save();
+    res.redircet('/')
+    }
+)
 
 module.exports = router;
